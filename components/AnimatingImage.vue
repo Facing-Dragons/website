2be220d7275a977de72e308793d5add3e7d27816
rajabiseraji <rajabiseraji@outlook.com>
2020-01-07 16:50:19 -0800

    feat(AniamtingImage): adds its props

--- components/AnimatingImage.vue
+++ components/AnimatingImage.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
+    <div
+        class="main-container"
+        :style=""
+    >
         <!-- This component basically works by this way: We will have a background picture and a foreground one
             The foreground is cropped to a size and by scrolling the size of the cropped image is going to 
             increase vertically
@@ -18,6 +21,14 @@ export default {
         // Any image put inside it must be resized to fill the height and width of the component
         containerHeight: String,
         containerWidth: String
+    },
+    computed: {
+        mainContainerStyleObject() {
+            return {
+                height: this.containerHeight,
+                width: this.containerWidth
+            }
+        }
     }
 }
 </script>
