7bbc51415d7e931aaef9b3be635df1c7c81fefac
rajabiseraji <rajabiseraji@outlook.com>
2020-01-14 02:33:03 -0800

    feat(AboutGame): adds the basics
    TODO: complete the component and add thematic stuff

--- components/AboutGame.vue
+++ components/AboutGame.vue
@@ -1,7 +1,21 @@
 <template>
-    <div class="team-section" :style="{background: tempBg}">
-        <div class="temp-text">{{tempText}}</div>
-    </div>    
+    <div class="row about-game-container">
+        <div class="about-title my-5 col-12 justify-content-center align-items-center d-flex text-center">
+            <strong>
+                Face Your Dragon <br>
+                <span class="red-text">
+                    Unlock Your Purpose
+                </span>    
+            </strong>
+        </div>
+        <div class="about-description col-12 justify-content-center align-items-center d-flex text-center">
+            <p>
+                Facing Dragons is a deeply gamified mental health app that helps young adults overcome life's challenges and 
+                unlock their purpose.
+            </p>
+        </div>
+
+    </div>
 </template>
 
 <script>
@@ -14,15 +28,19 @@ export default {
 </script>
 
 <style scoped>
-    .team-section {
-        width: 100%;
-        height: 400px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+    .red-text {
+        color: red;
     }
 
-    .temp-text {
-        font-size: 1.6rem;
+    .about-game-container {
+        color: white;
+    }
+
+    .about-title {
+        font-size: 2rem;
+    }
+
+    .about-description {
+        font-size: 1.5rem;
     }
 </style>
