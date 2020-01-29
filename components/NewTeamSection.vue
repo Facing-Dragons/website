88f67d79eeb797aae7cfbd61a810159661bab2f1
rajabiseraji <rajabiseraji@outlook.com>
2020-01-29 01:29:41 -0800

    build(VueAwesomeSwiper): adds the swiper

--- components/NewTeamSection.vue
+++ components/NewTeamSection.vue
@@ -14,6 +14,8 @@
 </template>
 
 <script>
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
 import SectionTitle from '~/components/SectionTitle';
 export default {
     components: {
