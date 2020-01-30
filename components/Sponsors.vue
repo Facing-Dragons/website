bcbccab8f234ec81508f4116201245dbcb4f043a
rajabiseraji <rajabiseraji@outlook.com>
2020-01-30 07:52:59 -0800

    fix(Sponsors): adds the links

--- components/Sponsors.vue
+++ components/Sponsors.vue
@@ -5,21 +5,25 @@
             right
             title-text="Our Sponsors"
             text-gradient="linear-gradient(to top, lightgrey 0%, lightgrey 1%, #e0e0e0 26%, #efefef 48%, #d9d9d9 75%, #bcbcbc 100%);"
-            line-gradient="linear-gradient(to left, black 0%, #f9d423 100%)"
+            line-gradient="linear-gradient(to right, black 0%, #f9d423 100%)"
         ></section-title>
         <div class="row justify-content-center justify-content-md-start">
             <div 
                 v-for="sponsor in sponsors"
                 :key="sponsor.title"
-                class="col-5 col-md-3 align-items-center"
+                class="col-5 col-md-3 align-items-center sponsor-image-col"
             >
-                <div class="bg-white shadow-sm rounded p-3 m-2 sponsor-image-div">
+                <a 
+                    class="bg-white rounded p-3 m-2 sponsor-image-div d-block"
+                    :href="sponsor.link"
+                    target="_blank"    
+                >
                     <img 
                         :src="sponsor.logo" 
                         :alt="sponsor.title"
                         class="sponsor-image"
                     >
-                </div>
+                </a>
             </div>
         </div>
     </b-container>
@@ -35,38 +39,47 @@ export default {
         const sponsors = [
             {
                 title: "cnrc",
+                link: "https://nrc.canada.ca/en",
                 logo: require("~/assets/img/cnrc.png")
             },
             {
                 title: "sfu",
+                link: "https://sfu.ca",
                 logo: require("~/assets/img/sfu.png")
             },
             {
                 title: "mitacs",
+                link: "https://www.mitacs.ca/en",
                 logo: require("~/assets/img/mitacs.jpg")
             },
             {
                 title: "columbia trust",
+                link: "https://ourtrust.org/",
                 logo: require("~/assets/img/columbiabasin.jpg")
             },
             {
                 title: "innovate bc",
+                link: "https://innovatebc.ca/",
                 logo: require("~/assets/img/innovatebc.png")
             },
             {
                 title: "new ventures bc",
+                link: "https://www.newventuresbc.com/",
                 logo: require("~/assets/img/newventure.png")
             },
             {
                 title: "ryla",
+                link: "http://ryla5040.org/",
                 logo: require("~/assets/img/RYLA.jpg")
             },
             {
                 title: "kpu",
+                link: "https://kpu.ca",
                 logo: require("~/assets/img/kpu.jpg")
             },
             {
                 title: "kast",
+                link: "https://kast.com/",
                 logo: require("~/assets/img/kast.png")
             },
         ];  
@@ -77,14 +90,16 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
     .sponsor-image {
         width: 100%;
         height: auto;
     }
-
     .sponsor-image-div {
         transition: all 0.3s ease-in-out;
+        -webkit-box-shadow: 4px 9px 11px -2px rgba(255, 255, 255,0.75);
+        -moz-box-shadow: 4px 9px 11px -2px rgba(255, 255, 255,0.75);
+        box-shadow: 4px 9px 11px -6px rgba(255, 255, 255,0.75);
     }
 
     .sponsor-image-div:hover {
