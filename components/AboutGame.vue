a58596c15b8f7a84aa6512ce887816272fb9e5d9
rajabiseraji <rajabiseraji@outlook.com>
2020-01-31 16:56:52 -0800

    fix(AboutGame): updates the video

--- components/AboutGame.vue
+++ components/AboutGame.vue
@@ -6,10 +6,13 @@
                     <b-embed
                         type="iframe"
                         aspect="16by9"
-                        src="https://www.youtube.com/embed/TA1uyIciJ8Y"
+                        src="https://www.youtube.com/embed/Pd8N8NEi6j4?rel=0"
                         allowfullscreen
                     ></b-embed>
                 </div>
+            </div>
+            <div class="col-12 justify-content-between align-items-center game-features-section">
+                
             </div>
         </div>
     </div>
