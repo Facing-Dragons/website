91678cd4885bf5f3c73a368e24a5868d4656978c
rajabiseraji <rajabiseraji@outlook.com>
2020-02-12 19:23:53 -0800

    build(firebase): adds firebase to the project

--- components/Banner.vue
+++ components/Banner.vue
@@ -22,7 +22,7 @@
                 <b-card class="msg-container d-flex flex-column justify-content-center p-2">
                   <p class="overlay-card-text">Get Your First Quest!</p>
                   <!-- Begin Mailchimp Signup Form -->
-                    <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+                    <!-- <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
                     <style type="text/css">
                       #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
                       /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
@@ -33,15 +33,16 @@
                         <div id="mc_embed_signup_scroll">
                       <label for="mce-EMAIL">Subscribe</label>
                       <input type="email" v-model="form.email" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-                        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups -->
-                        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3f352ef5878b1f137638588d9_e46afd87ef" tabindex="-1" value=""></div>
+                        real people should not fill this in and expect good things - do not remove this or risk form bot signups
+                        <div style="position: absolute; left: -5000px;" aria-hidden="true"><inp
+                        ut type="text" name="b_3f352ef5878b1f137638588d9_e46afd87ef" tabindex="-1" value=""></div>
                         <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
                         </div>
                     </form>
-                    </div>
+                    </div> -->
 
                     <!--End mc_embed_signup-->
-                  <!-- <b-form action="https://github.us4.list-manage.com/subscribe/post?u=3f352ef5878b1f137638588d9&amp;id=e46afd87ef" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                  <b-form action="https://github.us4.list-manage.com/subscribe/post?u=3f352ef5878b1f137638588d9&amp;id=e46afd87ef" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                     <b-form-group
                       id="input-email-group"
                       :state="emailState"
@@ -56,8 +57,8 @@
                         placeholder="Enter Your Email"
                       ></b-form-input>
                     </b-form-group>
-                  </b-form> -->
-                  <!-- <div class="modal-button-container w-100 d-flex justify-content-end">
+                  </b-form> 
+                  <div class="modal-button-container w-100 d-flex justify-content-end">
                     <b-button variant="danger" @click="handleOverlayClick">No Thanks!</b-button>
                     <b-button 
                       class="ml-2" 
@@ -66,7 +67,7 @@
                     >
                         Submit
                     </b-button>
-                  </div> -->
+                  </div>
                 </b-card>
               </div>
           </div>
