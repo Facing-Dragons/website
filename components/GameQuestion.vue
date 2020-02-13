91c59cd50de30269412c6a2f772dbdabc875f855
rajabiseraji <rajabiseraji@outlook.com>
2020-02-12 21:07:22 -0800

    feat(GameQuestion): Adds the slider

--- components/GameQuestion.vue
+++ components/GameQuestion.vue
@@ -1,35 +1,75 @@
 <template>
-    <div class="container main-quest-container">
+    <div class="container main-quest-container h-100 d-flex justify-content-center align-items-center">
         <div class="row">
+            <div class="col-12 justify-content-center">
+                <h1 class="quest-title">
+                    {{ title }}
+                </h1>
+            </div>
             <div class="col-12 justify-content-center">
                 <p class="quest-text">
                     {{ text }}
                 </p>
             </div>
-            <div class="col-12 justify-content-center align-items-center">
-                <knob-control 
+            <div class="col-12 justify-content-center align-items-center control-col">
+                <vue-slider
                     v-model="value"
-                    :min="0"
-                    :max="10"
-                ></knob-control>
+                    :adsorb="true"
+                    :interval="10"
+                    :marks="true"
+                    :dotSize="18"
+                    height="20px"
+                    :processStyle="computedStyle"
+                    :data="[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
+                ></vue-slider>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import KnobControl from 'vue-knob-control'
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/default.css'
+
 export default {
     components: {
-        KnobControl,
+        VueSlider,
     },
     props: {
         text: String,
-        value: Number
+        value: Number,
+        color: String,
+        title: String,
+    },
+    computed: {
+        computedStyle() {
+            return {
+                backgroundColor: this.color,
+                opacity: this.value/10,
+                transition: '0.5s all ease-in-out'
+            }
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
+.control-col {
+    // height: 40vh;
+}
 
+.quest-title {
+    font-size: 12vw;
+    text-align: center;
+    font-weight: 200;
+}
+
+.quest-text {
+    font-size: 2vw;
+    font-weight: 300;
+    text-align: center;
+    letter-spacing: 0.1vw;
+    color: #1e1e1e;
+    margin-bottom: 10vh;
+}
 </style>
