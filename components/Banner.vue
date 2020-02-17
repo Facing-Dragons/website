9f7df6d1f5d29aab0965baa624e525cff773ecac
rajabiseraji <rajabiseraji@outlook.com>
2020-02-16 18:44:58 -0800

    fix(Banner): fixes the title sizing

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,5 +1,6 @@
 <template>
-  <b-container fluid class="main d-flex align-items-md-center">
+  <b-container fluid class="main d-flex align-items-lg-center">
+    <!-- This is the Email Modal Section -->
     <transition name="fade">
           <div 
             v-show="isPlayerModalOpen"
@@ -119,13 +120,12 @@
     <div class="title-box">
       <div class="main-title">
         <banner-text-section></banner-text-section>
-        <banner-text-unlock class="my-4"></banner-text-unlock>
+        <banner-text-unlock class="mt-4"></banner-text-unlock>
       </div>
-      <p class="small-text mt-0 pt-0 mt-md-4 pt-md-4">
-        Facing Dragons is the first mixed-reality
-        Mental health and career support game. 
-      </p>
-      <div class="button-container d-flex flex-column p-0 pt-5 justify-content-center align-items-start">
+      <div class="title-description">
+        <title-desc></title-desc>
+      </div>
+      <div class="button-container d-flex flex-column p-0 justify-content-center align-items-start">
         <button 
           class="mb-2 py-2 custom-buttons w-100 w-md-75 rounded"
           :class="{'nav-button': isButtonInNav}"
@@ -155,6 +155,7 @@
  */
 import BannerTextSection from '~/components/BannerTextFirst'
 import BannerTextUnlock from '~/components/BannerTextUnlock'
+import TitleDesc from '~/components/TitleDesc'
 import AnimatingArrowDown from '~/components/AnimatingArrowDown'
 import {fireDb} from '~/plugins/firebase.js'
 
@@ -163,6 +164,7 @@ export default {
       BannerTextSection,
       BannerTextUnlock,
       AnimatingArrowDown,
+      TitleDesc
     },
     props: {
       isSupport: {
@@ -290,26 +292,6 @@ export default {
   z-index: 4;
 }
 
-.mobile-support {
-  width: 100%;
-  position: fixed;
-  bottom: 0;
-  right: 0;
-  left: 0;
-}
-
-.support-text * {
-  font-weight: 700;
-}
-
-.support-icon {
-  font-size: 5vw;
-  animation-name: clickAnimation;
-  animation-duration: 1.5s;
-  animation-timing-function: ease-in-out;
-  animation-iteration-count: infinite;
-}
-
 @keyframes clickAnimation {
   0% {
     transform: scale(1) rotateY(0);
@@ -433,33 +415,10 @@ export default {
   width: 4vmin;
 }
 
-.secondary {
-  height: 100vh;
-  position: absolute;
-  top: 0;
-  width: 100%;
-}
-
 .main {
   height: 100vh;
   position: relative;
   top: 0;
-}
-
-.main-container {
-  height: 300vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-}
-
-.dragon-image, .mountain-image {
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  height: auto;
-  width: 50vw;
 }
 
 .title-box {
@@ -468,78 +427,15 @@ export default {
   position: relative;
 }
 
-.logo-container {
-  position: absolute;
-  display: flex;
-  top: 3vw;
-  background: black;
-  left: 24.7vw;
-}
-
-.title-logo {
-  width: 6.5vw;
-}
-
-.main-title-text {
-  color: #f79519;
-  font-size: 8vw;
-  font-weight: 500;
-  background-image: linear-gradient(to right, #f83600 0%, #f9d423 100%);
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-}
-
-.main-title {
-  /* margin-top: -3.7vw; */
+.main-title, .title-description {
   max-width: 40vw;
 }
 
-.second {
-  margin-top: -6.1vw;
-  position: relative;
-}
-
-.pre-title {
-  padding-left: 0.5vw;
-  letter-spacing: 0.8vw;
-  font-size: 1.7vw;
-  font-weight: 100;
-  background-image: linear-gradient(to top, lightgrey 0%, lightgrey 1%, #e0e0e0 26%, #efefef 48%, #d9d9d9 75%, #bcbcbc 100%);
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-}
-
-#purpose {
-  /* color: #ebebeb; */
-  background-image: linear-gradient(to right, #92fe9d 0%, #00c9ff 100%);
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  font-weight: 100;
-}
-
-.your-title {
-  background-image: linear-gradient(to right, #92fe9d 0%, #00c9ff 100%);
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  margin-left: -3vw;
-  font-size: 2vh;
-}
-
-.small-text {
-  font-weight: 500;
+.title-description {
   opacity: 0;
-  text-transform: uppercase;
-  /* margin-left: 1vw; */
-  font-size: max(1.9vh, 1.6vw);
-  color: white;
-  letter-spacing: 0.2vw;
-  text-align: justify;
-  max-width: 40vw;
   animation: fade-and-up 1.5s forwards ease-in-out 3s;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
 }
 
 @keyframes fade-and-up {
@@ -576,24 +472,6 @@ export default {
   background: #fae667;
 }
 
-text {
-  fill: #00c9ff;
-}
-
-#face {
-  fill: linear-gradient(to right, #f83600 0%, #f9d423 100%);
-}
-
-#your {
-  position: absolute;
-  width: 10vw;
-  left: 17vw;
-  top: 17vh;
-}
-
-#dragon {
-  /* margin-top: -7.5vh; */
-}
 
 @media screen and (max-width: 5870px) {
   .title-box {
@@ -601,111 +479,60 @@ text {
   }
 }
 
-@media screen and (max-width: 500px) {
+@media screen and (max-width: 1025px) {
+  .title-box {
+    position: absolute;
+    bottom: 5vh;
+    max-width: 55vw;
+  }
+
+  .button-container {
+    max-width: 100%;
+  }
+
+
+  .main-title, .title-description {
+    max-width: 100%;
+  }
+}
+
+@media screen and (max-width: 767px) {
+  .title-box {
+    position: absolute;
+    bottom: 5vh;
+    max-width: 40vw;
+  }
+
+  .main-title, .title-description {
+    max-width: 100%;
+  }
+}
+
+@media screen and (max-width: 576px) {
   * {
     transition: all 0.5ms ease-in-out;
   }
 
   .button-container {
-    position: relative;
-    top: 20vh;
+    position: absolute;
+    max-width: 100vw;
+    /* top: 20vh; */
+    width: 100%;
+    bottom: 4vh;
   }
 
-  /* .custom-buttons {
-    font-size: 1.5rem;
-  } */
-
-  .main-title {
-    /* margin-top: -3.7vw; */
+  .main-title, .title-description {
     max-width: 90vw;
   }
 
-  .dragon-image {
-    right: -31vw;
-    height: 43vh;
-    width: auto;
-  }
-
-  .small-text {
-    max-width: 100%;
-    font-size: max(1.5vh, 1.2vw);
-  }  
-
   .title-box {
-    /* margin-top: 20vh;
-    width: 100%; */
+    position: relative;
+    max-width: 90vw;
     transition: 0.2s all ease-in-out;
-  }
-
-  .pre-title {
-    padding-left: 0.6vw;
-    letter-spacing: 0.8vw;
-    font-size: 3.7vw;
   }
 
   .main-title {
     margin-top: -3vh;
-  }
-
-  .main-title-text {
-    font-size: 7.7vh;
-  }
-
-  .second {
-    margin-top: -6vh;
-    position: relative;
-    font-size: 7.7vh;
-  }
-
-  .logo-container {
-    display: flex;
-    top: 2.5vh;
-    height: 7vh;
-    background: black;
-    left: 24vh;
-  }
-
-  .title-logo {
-    position: relative;
-    top: 0.5vh;
-    height: 6vh;
-    width: auto;
-  }
-}
-
-@media screen and (max-width: 480px) {
-   .main-title {
-    margin-top: -3vh;
-  }
-
-  .main-title-text {
-    font-size: 6vh;
-  }
-
-  .second {
-    margin-top: -4.4vh;
-    position: relative;
-    font-size: 6vh;
-  }
-
-  .button-container {
-    max-width: 100%;
-    bottom: 10vh;
-  }
-
-  .logo-container {
-    display: flex;
-    top: 2.1vh;
-    height: 7vh;
-    background: black;
-    left: 18.6vh;
-  }
-
-  .title-logo {
-    position: relative;
-    top: 0;
-    height: 5vh;
-    width: auto;
   }
 }
 </style>
