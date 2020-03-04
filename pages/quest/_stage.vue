c563c20d183d51f43c7721459f0d5af254725b10
rajabiseraji <rajabiseraji@outlook.com>
2020-03-04 15:29:23 -0800

    feat(quest): adds games difficulty desc

--- pages/quest/_stage.vue
+++ pages/quest/_stage.vue
@@ -12,7 +12,7 @@
                 <component :is="currentTextComponent"></component>
             </transition>
         </game-question>
-      <div class="button-container d-none d-md-block">
+      <!-- <div class="button-container d-none d-md-block">
         <transition
           name="slide-fade"
         >
@@ -37,11 +37,11 @@
           </div>
         </div>
         </transition>
-      </div>
+      </div> -->
 
       <!-- MOBILE ARROWS -->
 
-      <div class="mobile-button-container d-block d-md-none">
+      <!-- <div class="mobile-button-container d-block d-md-none">
         <transition
           name="slide-fade"
         >
@@ -64,7 +64,7 @@
           <chevron-right class="arrow-icon chevron-right" fill="black" ></chevron-right>
         </div>
         </transition>
-      </div>
+      </div> -->
     </div>
 </template>
 
@@ -84,6 +84,7 @@ import {fireDb} from '~/plugins/firebase.js'
 import {mapState} from 'vuex';
 
 export default {
+  layout: 'quest',
   components: {
     GameQuestion,
     ChevronRight,
@@ -149,6 +150,8 @@ export default {
         title: 'Mission',
         titleComponent: 'MissionText',
         text: 'How fulfilled are you in knowing and living your mission, work that is meaningful to you?',
+        lowDescription: 'Struggling to figure out what to do with your life, under pressure to find a meaningful career path, stressed out working a job you hate',
+        highDescription: 'Enjoying work so much it doesn’t feel like work, making a difference or positive impact in the world, fully engaged in meaningful and satisfying work.',
         value: randomGameScores.missionScore,
         color: "#ff6800"
       },
@@ -156,6 +159,8 @@ export default {
         title: 'Mind',
         titleComponent: 'MindText',
         text: `How fulfilled are you in your mental and emotional health?`,
+        lowDescription: 'High stress, low resilience, unhealthy beliefs and focus, depressed, anxious, low self-esteem, low confidence.',
+        highDescription: 'Low stress, high resilience, healthy beliefs and focus, clear-minded, at peace, confident, centered, grounded.',
         value: randomGameScores.mindScore,
         color: "#0059b9"
       },
@@ -163,6 +168,8 @@ export default {
         title: 'Fun',
         titleComponent: 'FunText',
         text: 'How fulfilled are you in having fun, being playful, and enjoying the lighter side of yourself and your life?',
+        lowDescription: 'Not having fun, feeling miserable and stuck, too serious, all work and no play.',
+        highDescription: 'Having fun, feeling relaxed and enjoying your life, smiling and laughing a lot, engaged in extracurricular activities, hobbies and recreation, feeling playful and happy, good work/life balance.',
         value: randomGameScores.funScore,
         color: "#f9e777"
       },
@@ -170,6 +177,8 @@ export default {
         title: 'Social',
         titleComponent: 'SocialText',
         text: 'How are you doing at staying in touch and feeling connected with the people who mean the most to you?',
+        lowDescription: 'Not feeling connected to the people around you, feeling isolated, lonely, struggling to make and maintain friendships, feeling betrayed, low trust in others.',
+        highDescription: 'Feeling connected to the people around you, enjoying a rich, satisfying social life with people you trust and enjoy spending time with.',
         value: randomGameScores.socialScore,
         color: "#61a5e3"
       },
@@ -177,6 +186,8 @@ export default {
         title: 'Home',
         titleComponent: 'HomeText',
         text: 'How fulfilled are you with your home and the current state of your physical surroundings?',
+        lowDescription: 'You don’t like your home, don’t have a home, home is a mess, don’t feel like your surroundings are a true reflection of who you are.',
+        highDescription: 'You enjoy your home and the world around you, home is comfortable, safe, clean and organized. Your surroundings are a true reflection of who you are.',
         value: randomGameScores.homeScore,
         color: "#72655f"
       },
@@ -184,6 +195,8 @@ export default {
         title: 'Love',
         titleComponent: 'LoveText',
         text: 'How fulfilled are you in experiencing feelings of intimacy, love for self and others, and actively living with love?',
+        lowDescription: 'Given up on love or finding love, feeling hurt from a break-up or relationship, struggling to feel, express and receive love.',
+        highDescription: 'In love, feeling content with current relationship situation, enjoying feeling, expressing and receiving love.',
         value: randomGameScores.loveScore,
         color: "#c22832"
       },
@@ -191,6 +204,8 @@ export default {
         title: 'Wealth',
         titleComponent: 'WealthText',
         text: ' How are you doing at having enough money to do what you want, enjoying financial security, and having the skills and mindset to grow your wealth?',
+        lowDescription: 'Money is an obstacle, stressed about money, in debt with no foreseeable way out, not sticking to a budget, getting by on credit cards, bills piling up, spending more than you make.',
+        highDescription: 'Money is a tool, feeling affluent, in control of your finances, making more than you spend, saving for the future, making smart investments, working with a budget.',
         value: randomGameScores.wealthScore,
         color: "#FFD700"
       },
@@ -198,6 +213,8 @@ export default {
         title: 'Vitality',
         titleComponent: 'VitalityText',
         text: 'How fulfilled are you in your physical health and well-being, and having the energy to do the things you want?',
+        lowDescription: 'Low energy, little movement, poor food and drink choices, feeling weak, lethargic, and unhealthy.',
+        highDescription: 'Lots of energy and movement, healthy food and drink choices, feeling strong and energized.',
         value: randomGameScores.vitalityScore,
         color: "#76b72b"
       }
@@ -240,6 +257,8 @@ export default {
         value: this.gameQuestions[this.currentStepIndex].value,
         title: this.gameQuestions[this.currentStepIndex].title,
         question: this.gameQuestions[this.currentStepIndex].question || '',
+        lowDescription: this.gameQuestions[this.currentStepIndex].lowDescription || '',
+        highDescription: this.gameQuestions[this.currentStepIndex].highDescription || '',
       }
     },
     currentTextComponent() {
@@ -251,7 +270,6 @@ export default {
 
 <style scoped>
   .main-container {
-    background: #ebebeb;
     width: 100vw;
     /* height: 100vh; */
     top: 0;
