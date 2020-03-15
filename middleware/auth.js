faa5527abb5d5232150ebe8e7720337d3804697c
rajabiseraji <rajabiseraji@outlook.com>
2020-03-15 14:43:45 -0400

    fix(auth): fixes some auth and redirect bugs

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,11 @@
-export default function({store, redirect}) {
+export default function({store, redirect, app}) {
+    app.$fireAuth.onAuthStateChanged(function (user) {
+        if (user) {
+            console.log(user);
+        } else {
+            return redirect('/quest/access');
+        }
+    });
     console.log(store.state.authUser);
     store.dispatch('quest/getUser');
     if (!store.state.authUser || !store.state.authUser.uid || !store.state.authUser.email) {
