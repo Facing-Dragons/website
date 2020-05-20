9a7c4ca4e030583a9a35e0c68a77c6c95c94a93b
rajabiseraji <rajabiseraji@outlook.com>
2020-05-20 14:35:35 -0700

    b4dec5b

---
+++ playerTypes.js
@@ -0,0 +1,276 @@
+const playerTypes = {
+    mission: {
+        mission: {
+            title: "",
+            text: ""
+        },
+        mind: {
+            title: "Prophet",
+            text: "Tormented by clarity"
+        },
+        fun: {
+            title: "Martyr",
+            text: "The path isn't always easy"
+        },
+        social: {
+            title: "Lone Wolf",
+            text: "You'd only get in my way"
+        },
+        home: {
+            title: "Missionary",
+            text: "I follow the true path"
+        },
+        love: {
+            title: "Commander",
+            text: "Foused on the mission"
+        },
+        wealth: {
+            title: "Priest",
+            text: "Vow of poverty?"
+        },
+        vitality: {
+            title: "Master",
+            text: "Stay a while and listen"
+        }
+    },
+    mind: {
+        mission: {
+          title: "Illusionist",
+          text: "I can imagine many realities"
+        },
+        mind: {
+          title: "",
+          text: ""
+        },
+        fun: {
+          title: "Archivist ",
+          text: "Shhhh!"
+        },
+        social: {
+          title: "Mad scientist",
+          text: "Get out of my lab!"
+        },
+        home: {
+          title: "Wind Mage",
+          text: "The wind calls my name"
+        },
+        love: {
+          title: "Guard Captain ",
+          text: "Very disciplned"
+        },
+        wealth: {
+          title: "Guild Master",
+          text: "Rich in knowledge"
+        },
+        vitality: {
+          title: "Professor",
+          text: "Doing great upstairs"
+        }
+    },
+    fun: {
+        mission: {
+          title: "Chaos mage",
+          text: "I don 't cast spells, they cast me"
+        },
+        mind: {
+          title: "Assassin",
+          text: "I can 't say I'm bored"
+        },
+        fun: {
+          title: "",
+          text: ""
+        },
+        social: {
+          title: "Duelist",
+          text: "It is fun to win"
+        },
+        home: {
+          title: "Adventurer",
+          text: "Where 's next?!"
+        },
+        love: {
+          title: "Pirate",
+          text: "YARR!!"
+        },
+        wealth: {
+          title: "Jester",
+          text: "someone buy me a drink!"
+        },
+        vitality: {
+          title: "Lightning Wizard",
+          text: "Glass cannon"
+        }
+    },
+    social: {
+        mission: {
+          title: "Court Noble",
+          text: "What a lovely dress!"
+        },
+        mind: {
+          title: "Spy",
+          text: "Keep it secret. Keep it safe"
+        },
+        fun: {
+          title: "Politican",
+          text: "Says what is needed"
+        },
+        social: {
+          title: "",
+          text: ""
+        },
+        home: {
+          title: "Socialite",
+          text: "Let's go out!"
+        },
+        love: {
+          title: "Friend",
+          text: "I think of you like a brother"
+        },
+        wealth: {
+          title: "Bard",
+          text: "Toss a coin to your witcher"
+        },
+        vitality: {
+          title: "Inkeeper",
+          text: "Busy keeping people fed"
+        }
+    },
+    home: {
+        mission: {
+          title: "Villager",
+          text: "I do stuff, and live here"
+        },
+        mind: {
+          title: "Homebody",
+          text: "Why leave home?"
+        },
+        fun: {
+          title: "Worker",
+          text: "I made this"
+        },
+        social: {
+          title: "Hermit",
+          text: "Ahh, the sound of silence"
+        },
+        home: {
+          title: "",
+          text: ""
+        },
+        love: {
+          title: "Cat Herder",
+          text: "Not really alone, technically"
+        },
+        wealth: {
+          title: "Homesteader",
+          text: "Home is where you make it"
+        },
+        vitality: {
+          title: "Village Elder",
+          text: "I enjoy it here"
+        }
+    },
+    love: {
+        mission: {
+          title: "Glade Unicorn",
+          text: "Oooo, pretty!"
+        },
+        mind: {
+          title: "Virtuoso",
+          text: "Feelings flow deeply"
+        },
+        fun: {
+          title: "Paladin",
+          text: "Serious about love"
+        },
+        social: {
+          title: "Devotee",
+          text: "One before all others"
+        },
+        home: {
+          title: "Travelling healer",
+          text: "I go where I can help most"
+        },
+        love: {
+          title: "",
+          text: ""
+        },
+        wealth: {
+          title: "Monk",
+          text: "Cares deeply; needs little"
+        },
+        vitality: {
+          title: "Grandma/Grandpa",
+          text: "The heart is the strongest muscle"
+        }
+    },
+    wealth: {
+        mission: {
+          title: "Heir/Heiress",
+          text: "What, me worry?"
+        },
+        mind: {
+          title: "Dark Lord/Lady",
+          text: "Doing great! At least financially…"
+        },
+        fun: {
+          title: "Vault Keeper",
+          text: "Look at all this boring treasure!"
+        },
+        social: {
+          title: "King/Queen of The North",
+          text: "Leave my castle at once!"
+        },
+        home: {
+          title: "Spice Trader",
+          text: "My cart is heavy with wares"
+        },
+        love: {
+          title: "Treasure Golem",
+          text: "Yearns for something that money can't buy"
+        },
+        wealth: {
+          title: "",
+          text: ""
+        },
+        vitality: {
+          title: "Jeweled Skeleton",
+          text: "What it lacks in muscle, it makes up for in bling!"
+        }
+    },
+    vitality: {
+        mission: {
+          title: "Sellsword",
+          text: "Ready for action"
+        },
+        mind: {
+          title: "Berserker",
+          text: "RAAAAA!!!"
+        },
+        fun: {
+          title: "Officer",
+          text: "Seriously disciplined"
+        },
+        social: {
+          title: "Ranger",
+          text: "I keep my own council"
+        },
+        home: {
+          title: "Ronin",
+          text: "A wandering samurai with no master"
+        },
+        love: {
+          title: "Warrior",
+          text: "All is fair in love and war"
+        },
+        wealth: {
+          title: "Back Alley Brawler",
+          text: "Fighting for a break"
+        },
+        vitality: {
+          title: "",
+          text: ""
+        }
+    }
+};
+
+export default playerTypes;
